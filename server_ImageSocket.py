3ec3537e221d701a7e8bc5a666573a9085cbcc41
nick825319 <nick825319@gmail.com>
2020-03-30 09:57:40 +0800

    Update server_ImageSocket.py

--- server_ImageSocket.py
+++ server_ImageSocket.py
@@ -1,5 +1,4 @@
 import socket
-import sys, errno
 hostname = '127.0.0.1'
 port = 7777
 image_name = "image%s.png"
@@ -16,7 +15,6 @@ connect_socket, client_address = server.accept()
 print(client_address, "connecting")
 
 while True:
-    txt = None
     print("waiting for new event")
     receive_content = connect_socket.recv(buffer_size)
     txt = receive_content.decode("utf-8")
@@ -26,30 +24,27 @@ while True:
 
         print('size is %s' % size)
         connect_socket.send(bytes("GOT SIZE", encoding="utf-8"))
-        # Now set the buffer size for the image
+        # Set the buffer size for the image
         print('got size')
         buffer_size = 40960000
 
-        my_file = open(image_name % imgcounter, 'ab')
+        # write file
+        my_file = open(image_name % imgcounter, 'wb')
         while True:
             receive_content = connect_socket.recv(buffer_size)
             my_file.write(receive_content)
             if not receive_content:
                 break
-            print("write file finish")
+            print("writing to file finished")
             my_file.close()
-            connect_socket.send(bytes("GOT IMAGE", encoding="utf-8"))
-            print("send got image ")
+            connect_socket.send(bytes("SERVER GOT IMAGE", encoding="utf-8"))
+
+            buffer_size = 4096
+            imgcounter += 1
             break
     elif txt.startswith('BYE'):
-        connect_socket.shutdown()
-    if not receive_content:
-        buffer_size = 4096
-        connect_socket.send(bytes("server get content success", encoding="utf-8"))
-        print("no content...")
-        break
+        connect_socket.close()
 
 
-print("close connect")
 connect_socket.close()
 
