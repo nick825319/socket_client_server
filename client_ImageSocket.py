d01c6425ba70af9caee9bae934dd0818878d4685
nick825319 <nick825319@gmail.com>
2020-06-18 15:43:31 +0800

    Update client_ImageSocket.py

--- client_ImageSocket.py
+++ client_ImageSocket.py
@@ -2,7 +2,7 @@ import socket
 
 ip = '127.0.0.1'
 
-port = 7777
+port = 12000
 
 client_socket = socket.socket()
 client_socket.connect((ip, port))
@@ -12,13 +12,14 @@ image_name = "image%s.png"
 
 while True:
     content = input("image name (add file Extension name, '-1' for disconnect):")
+    print(content)
     if content is None or content == "":
         content = image_name = "image%s.png" % 1
-    elif content == -1:
-        client_socket.send(bytes("BYE", encoding='utf-8'))
-        client_socket.close()
+    elif content == "-1":
+        client_socket.send(bytes("BYE", encoding='ascii'))
         break
-
+    else:
+        image_name = content
     # sending test message to server
     # client_socket.send(bytes(content, encoding='utf-8'))
     # recvdata = client_socket.recv(1024)
@@ -30,7 +31,7 @@ while True:
         size = len(file_bytes)
 
         # send size
-        client_socket.send(bytes("SIZE %s" % size, encoding='utf-8'))
+        client_socket.send(bytes("SIZE %s" % size, encoding='ascii'))
         recvdata = client_socket.recv(1024)
         print(str(recvdata))
         # send image bytes
@@ -38,7 +39,8 @@ while True:
             client_socket.sendall(file_bytes)
             recvdata = client_socket.recv(1024)
             print(str(recvdata))
-    except Exception:
+    except OSError as e:
+        print(e.args)
         pass
 
 
